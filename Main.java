eb65e5f8bba83e00c3ea59c6a474aaa73ccb3a94
kacpersoltys2 <126696092+kacpersoltys@users.noreply.github.com>
2023-03-15 15:35:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args)
+  {
+    int sum = 0;
+    for(int i=0; i<=100; i+=2)
+        sum += i;
+      {
+        System.out.println("Suma liczb parzystych z przedziału <0, 100> = " + sum);
+      }
+  }
+}
